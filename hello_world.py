da0a58234ea23780b97851074091b4230a9a8cbf
Angel Rivera <arivera@microshare.io>
2018-05-31 00:03:53 -0400

    fixed data

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,21 @@
 from flask import Flask
 
 app = Flask(__name__)
+def wrap_html(text):
+    msg = """
+        <html>
+            <div style='font-size:120px;'>
+            <center>
+                {0}
+            </center>
+            </div>
+        </html>
+    """.format(text)
+    return msg
 
 @app.route('/')
 def hello_world():
-    msg = """
-            <html>
-                <div style='font-size:120px;'>
-                <center>
-                    Hello World
-                </center>
-                </div>
-            </html>
-        """
+    msg = wrap_html('Hello World')
     return msg
 
 if __name__ == '__main__':
