1fcff266d7c03f2cc6d9f5107a90e843f6276cab
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2018-06-02 11:44:47 -0400

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def wrap_html(message):
+    html = """
+        <html>
+        <div style="font-size:120px">
+        <center>
+        <img height=200 width=800 src='https://infosiftr.com/wp-content/uploads/2018/01/unnamed-2.png'>
+        <br>
+        {0}
+        </center>
+        </div>
+        </html>""".format(message)
+    return html
+
+@app.route('/')
+def hello_world():
+    message = 'Hello DockerCon 2018'
+    html = wrap_html(message)
+    return html
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
