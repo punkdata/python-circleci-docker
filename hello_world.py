419a543e9f474125311044ecdd2e460da230798e
Angel Rivera <arivera@microshare.io>
2018-06-04 15:30:43 -0400

    initial commit

--- hello_world.py
+++ hello_world.py
@@ -1,21 +1,22 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 def wrap_html(message):
     html = """
-            <html>
-                <div style="font_size:120px">
-                    <center>
-                        <img height=200 width=800 src='https://infosiftr.com/wp-content/uploads/2018/01/unnamed-2.png'>
-                        {0}
-                    </center>
-                </div>
-            </html>""".format(message)
+        <html>
+            <div style="font-size:120px">
+            <center>
+                <img height=200 width=800 src='https://infosiftr.com/wp-content/uploads/2018/01/unnamed-2.png'>
+                {0}
+            </center>
+            </div>
+        </html>""".format(message)
     return html
 
 @app.route('/')
 def hello_world():
-    message = 'Hello DockerCon 2018!'
+    message = 'Hello DockerCon 2018'
     html = wrap_html(message)
     return html
 
--- test_hello_world.py
+++ test_hello_world.py
@@ -7,13 +7,13 @@ class TestHelloWorld(unittest.TestCase):
         self.app = hello_world.app.test_client()
         self.app.testing = True
     
-    def test_response(self):
+    def test_default_status(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-    
-    def test_hello_message(self):
-        response = self.app.get('/')
-        self.assertEqual(response.data, hello_world.wrap_html('Hello DockerCon 2018!'))
 
-if __name__ == '__main__':
+    def test_message(self):
+        response = self.app.get('/')
+        self.assertEqual(response.data, 'Hello DockerCon 2018!')
+
+if __name__ = '__main__':
     unittest.main()
