5729f0c76f5451420e329da9694e81c4e957511e
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2018-06-11 08:28:44 -0700

    test demo

--- test_hello_world.py
+++ test_hello_world.py
@@ -10,10 +10,11 @@ class TestHelloWorld(unittest.TestCase):
     def test_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_message(self):
         response = self.app.get('/')
-        self.assertEqual(response.data, hello_world.wrap_html('Hello DockerCon 2018!'))
+        message = hello_world.wrap_html('Hello DockerCon 2018!')
+        self.assertEqual(response.data, message)
 
 if __name__ == '__main__':
     unittest.main()
