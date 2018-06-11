deb3fb7b8cced2ffe9812e721426bc9c715990d4
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2018-06-11 07:59:05 -0700

    fixed test typo

--- test_hello_world.py
+++ test_hello_world.py
@@ -13,7 +13,8 @@ class TestHelloWorld(unittest.TestCase):
     
     def test_messaged(self):
         response = self.app.get('/')
-        self.assertEqual(response.data, 'Hello DockerCon 2018!')
+        message = hello_world.wrap_html('Hello DockerCon 2018!')
+        self.assertEqual(response.data, message)
 
 if __name__ == '__main__':
     unittest.main()
