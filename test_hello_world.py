cb9890ea6372dd810d56cfc3535b05b4fea34df9
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2018-06-13 12:57:34 -0700

    tesat demo

---
+++ test_hello_world.py
@@ -0,0 +1,20 @@
+import hello_world
+import unittest
+
+class TestHelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.app = hello_world.app.test_client()
+        self.app.testing = True
+
+    def test_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_message(self):
+        response = self.app.get('/')
+        message = hello_world.wrap_html('Hello DockerCon 2018!')
+        self.assertEqual(response.data, message)
+
+if __name__ == '__main__':
+    unittest.main()
