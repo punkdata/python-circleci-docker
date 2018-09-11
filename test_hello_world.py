4e538bfe242bfbe6d3bf24d10604c9855f5a325b
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2018-09-11 19:42:12 -0400

    break it

--- test_hello_world.py
+++ test_hello_world.py
@@ -13,7 +13,7 @@ class TestHelloWorld(unittest.TestCase):
     
     def test_message(self):
         response = self.app.get('/')
-        message = hello_world.wrap_html('Hello PyLadies Chicago!')
+        message = hello_world.wrap_html('Hello DevOps DC Meetup!')
         self.assertEqual(response.data, message)
 
 if __name__ == '__main__':
