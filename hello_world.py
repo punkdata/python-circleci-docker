14463e383c7cb76fd236af452976003a83cb6981
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2018-09-11 23:34:24 -0400

    fixed devops dc meetup graphic

--- hello_world.py
+++ hello_world.py
@@ -8,7 +8,7 @@ def wrap_html(message):
         <body>
             <div style='font-size:80px;'>
             <center>
-                <image height="458" width="720" src="https://secure.meetupstatic.com/photos/event/2/a/a/3/600_452110915.jpeg">
+                <image height="458" width="720" src="https://secure.meetupstatic.com/photos/theme_body/9/a/8/e/full_7599566.jpeg">
                 <br>
                 {0}<br>
             </center>
